a6b0350eb822cd60d4f2237c01d44c7dcdc5e15a
David Gaspar <davidgaspar.dev@gmail.com>
2024-09-18 23:14:17 -0300

    refactor: Update CreateTask struct in taskvalueobject.go
    Update the CreateTask struct in taskvalueobject.go to include additional fields for resource code, cron code, start date, end date, and dependencies. The start and end dates are now of type time.Time instead of string. This change is necessary to enhance the functionality of the CreateTask feature.

--- internal/domain/valueobjects/taskvalueobject.go
+++ internal/domain/valueobjects/taskvalueobject.go
@@ -1,7 +1,22 @@
 package valueobjects
 
+import "time"
+
+/**
+- id: u64
+- name: string
+- resCode: string
+- cronCode: string
+- dateStart: Date
+- dateEnd: Date
+- deps: u64[]
+*/
+
 type CreateTask struct {
-	Name      string `json:"name"`
-	DateStart string `json:"dateStart"`
-	DateEnd   string `json:"dateEnd"`
+	Name      string    `json:"name"`
+	ResCode   string    `json:"resCode"`
+	CronCode  string    `json:"cronCode"`
+	DateStart time.Time `json:"dateStart"`
+	DateEnd   time.Time `json:"dateEnd"`
+	Deps      []uint64  `json:"deps"`
 }
